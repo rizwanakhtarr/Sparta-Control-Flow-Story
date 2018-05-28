7a2647b7014aabb1c94512a118b1c3d2df6261a6
Tech-A28 <tech-a28@tech-a28s-air.home>
2018-05-28 03:36:58 +0100

    Made a few changes to Quiz

---
+++ app.js
@@ -0,0 +1,48 @@
+
+var age = prompt("Are Sure you want to listen to this Scary Story?")
+if(age < 16) {
+alert("You are too Young for this scary story! Oh well");
+} else if (age >=16){
+alert("Proceed On!");
+}
+
+alert('Gray Forest was HAUNTED. One Rainy evening at 7, 3 Friends set out to prove to prove everyone it wasnt');
+
+
+
+
+alert('The friends names were...');
+
+let friends = ["Bruce","Tom", "Wendy"];
+alert(friends);
+
+alert('As they walked in the dark.a figure emerged from the trees and chased them. They Screameed!');
+
+
+
+var options = prompt('Do you think they should Run,Stay still,Attack,other options?')
+switch(options) {
+
+  case 'Attack':
+    alert('No Way MAAAN. Why');
+    break;
+    case 'Stay Still':
+      alert('pssssh really?');
+      break;
+      case 'Run':
+        alert('Yeeeas ofcourse. I would!');
+        break;
+
+      default:
+      alert('Hmmm Fair Enough..');
+}
+
+
+var skills = {
+  firstName:"Bruce"
+
+
+
+  color:"white"};
+
+alert(skills.firstName);
